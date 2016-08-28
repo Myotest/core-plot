49a403605ac8e46ba3a54b4c0a6afb4eb5a33c6a
Stefan Paychère <spaychere@myotest.com>
2016-08-28 17:07:12 +0200

    Update Package.swift
    Signed-off-by: Stefan Paychère <spaychere@myotest.com>

--- Package.swift
+++ Package.swift
@@ -2,5 +2,15 @@ import PackageDescription
 
 let package = Package(
     name: "core-plot",
-    exclude: ["build", "documentation", "examples", "framework", "QCPlugin", "READMEs", "scripts"]
+    exclude: ["build",
+              "documentation",
+              "examples",
+              "framework/CocoaPods",
+              "framework/English.lproj",
+              "framework/MacOnly",
+              "framework/TestResources",
+              "framework/xcconfig",
+              "QCPlugin",
+              "READMEs",
+              "scripts"]
 )
